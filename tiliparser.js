b0b181978a084895abb48b142533c0f95591167b
Marko Rintamäki <marko.rintamaki@gmail.com>
2016-10-18 15:41:07 +0300

    more instructions

--- tiliparser.js
+++ tiliparser.js
@@ -4,6 +4,10 @@
     Created By Marko Rintamäki (2016-10-18)
 
     Tiliparser parses Nordea account transfers and shows you useful monthly information
+
+    How to use:
+    - Get your account transfer data from Nordea bank and save it on your drive
+    - run 'node tiliparser.js <filename>' on the file
  */
 
 var fs = require('fs');
